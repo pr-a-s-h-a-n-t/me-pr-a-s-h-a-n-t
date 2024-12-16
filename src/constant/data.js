222f851d8dc930f07a2f69e29cab917ea5c0348a
prashant <prashantmishra111@outlook.com>
2024-12-17 00:55:10 +0530

    prd routing check

---
+++ src/constant/data.js
@@ -0,0 +1,32 @@
+export const skillsData = [
+  {
+    title: 'HTML5 & CSS3',
+    body: 'Experienced in building responsive and accessible user interfaces using modern HTML5 and CSS3.',
+    href: '#html-css-skills',
+  },
+  {
+    title: 'Syntactically Awesome Style Sheets(Sass)',
+    body: 'Proficient in writing maintainable and modular styles using Sass for scalable web design.',
+    href: '#sass-skills',
+  },
+  {
+    title: 'React.js',
+    body: 'Specialized in developing dynamic and interactive web applications with React.js, leveraging hooks, context, and routing.',
+    href: '#react-skills',
+  },
+  {
+    title: 'React Native',
+    body: 'Experienced in creating cross-platform mobile applications with React Native, integrating APIs, push notifications, and deep linking.',
+    href: '#react-native-skills',
+  },
+  {
+    title: 'Redux',
+    body: 'Efficient in managing application state using Redux Toolkit, Redux-Saga, and Redux-Thunk for complex workflows.',
+    href: '#redux-skills',
+  },
+  {
+    title: 'Next.js',
+    body: 'Experienced in building server-rendered and static web applications using Next.js for optimal performance.',
+    href: '#nextjs-skills',
+  },
+];
